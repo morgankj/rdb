f795a2d0aa122766be2d5b6610e000dbf534cf8b
Morgan Jones <morgan.kjones95@gmail.com>
2024-01-21 23:08:28 -0600

    Added images edited Home and Navbar and formatting

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,25 @@ import styled from "styled-components";
 import MainLogo from "../Images/MainLogo.png";
 
 export default function Home() {
-    return (
-      <p>HomePage</p>
-    );
+  return (
+    <StyledHome>
+      <div className="text">
+        <img alt="Ramen Del Barrio Main Logo" src={MainLogo} />
+        <h2>At RDB we combine Japanese and Mexican technique and  flavors to bring something new and exciting to Austin Diners. We specialize in an incredibly rich and thick tonkotsu broth paired with slow cooked Carnitas. Nothing about this is gluten free guey. Irrashaimasse y buen provecho cabrones!</h2>
+      </div>
+    </StyledHome>
+  );
+}
+
+const StyledHome = styled.div`
+.text {
+  padding: 5%;
+}
+  img {
+    width: 100%;
   }
+  h2 {
+    color: white;
+    font-size: 2rem;
+  }
+`
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,23 +1,57 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import rdblogo from "../Images/RDBsmall.png";
 
 export default function Navbar() {
-    return (
-        <StyledNavbar>
-            <a href="/" id="mainLogo">
-                <h3>RDB</h3>
-            </a>
-            <nav>
-                <ul>
-                    <li><Link to="/" >HOME</Link></li>
-                    <li><Link to="/menu" >MENU</Link></li>
-                    <li><Link to="/contact" >CONTACT</Link></li>
-                </ul>
-            </nav>
-        </StyledNavbar>
-    )
+  return (
+    <StyledNavbar>
+      <a href="/" id="mainLogo">
+        <img alt="RDP logo text" src={rdblogo} />
+      </a>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>
+            <Link to="/menu">MENU</Link>
+          </li>
+          <li>
+            <Link to="/contact">CONTACT</Link>
+          </li>
+        </ul>
+      </nav>
+    </StyledNavbar>
+  );
 }
 
 const StyledNavbar = styled.div`
-    
-`
+  display: flex;
+  justify-content: space-between;
+  padding: 1%;
+  #mainLogo {
+    width: 9rem;
+    display: flex;
+    align-items: center;
+    img {
+      width: 100%;
+    }
+  }
+  ul {
+    display: flex;
+    list-style-type: none;
+    font-family: "Caveat Brush", cursive;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 3rem;
+    li {
+      /* margin: 3%; */
+    }
+  }
+  a {
+    color: white;
+  }
+  a:hover {
+    color: red;
+  }
+`;
